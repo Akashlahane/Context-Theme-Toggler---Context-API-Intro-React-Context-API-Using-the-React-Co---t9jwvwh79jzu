c7b13a5e3a87b321c582a1eb97667d4944604930
Akashlahane <68777702+Akashlahane@users.noreply.github.com>
2023-08-31 06:15:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import { ThemeToggleButton } from "./ThemeToggleButton";
 import { Page } from "./Page";
 const AppContextApi = () => {
   return (
+
     <div id="main">
       <ThemeProvider>
         <Page />
         <ThemeToggleButton />
       </ThemeProvider>
     </div>
+    
   );
 };
 
